04a80eb25a80d67e60d500bb3a76f707c0d00b4e
binf01-Kambo <simonekambo1@gmail.com>
2025-04-28 19:48:54 +0200

    ho completato l'ultima pagina che ora mostra il dado uscito ed il testo hai vinto o hai perso

--- app/src/main/java/com/example/rollthedicekambo/MainActivity3.kt
+++ app/src/main/java/com/example/rollthedicekambo/MainActivity3.kt
@@ -1,6 +1,8 @@
 package com.example.rollthedicekambo
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,5 +14,24 @@ class MainActivity3 : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main3)
 
+        val mioRandom = intent.getIntExtra("RANDOM", -1)
+        val input = intent.getIntExtra("INPUT", -1)
+        val textViewRisultato = findViewById<TextView>(R.id.textViewRisultato) //campo di testo per il risultato
+        val dadi = findViewById<ImageView>(R.id.dadi)
+        val immaginiDadi = when(mioRandom){
+            1 -> R.drawable.dice_face_1
+            2 -> R.drawable.dice_face_2
+            3 -> R.drawable.dice_face_3
+            4 -> R.drawable.dice_face_4
+            5 -> R.drawable.dice_face_5
+            else -> {R.drawable.dice_face_6}
+        }
+        dadi.setImageResource(immaginiDadi as Int)
+        if(mioRandom == input){
+            textViewRisultato.text = "Hai vinto!"
+        }
+        else{
+            textViewRisultato.text = "Hai perso!"
+        }
     }
 }
