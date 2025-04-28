a332b85ec90e7c1bb7908f75bec93b564fe84e01
binf01-Kambo <simonekambo1@gmail.com>
2025-04-28 19:03:01 +0200

    ho aggiunto il titolo, l'immagine dei dadi e un pulsante continua che richiama il secondo activity main

--- app/src/main/java/com/example/rollthedicekambo/MainActivity.kt
+++ app/src/main/java/com/example/rollthedicekambo/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.rollthedicekambo
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,5 +18,18 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val textViewTitle = findViewById<TextView>(R.id.textViewTitle)
+        val immagineDado = findViewById<ImageView>(R.id.immagineDado)
+        val continuaButton = findViewById<Button>(R.id.continuaButton)
+
+        continuaButton.setOnClickListener(View.OnClickListener {
+            val mioToast = Toast.makeText(this, "Continua", Toast.LENGTH_LONG)
+            mioToast.show()
+            intent()
+        })
+    }
+    private fun intent(){
+        val mioIntent = Intent(this, MainActivity2::class.java)
+        startActivity(mioIntent)
     }
 }
