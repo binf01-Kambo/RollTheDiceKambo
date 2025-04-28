ca6b1b56048de4694fcd7ddf820c9240aee09538
binf01-Kambo <simonekambo1@gmail.com>
2025-04-28 19:30:38 +0200

    ho aggiunto la condizione per verificare che il numero inserito sia valido, in quel caso passo al terzo activity main

--- app/src/main/java/com/example/rollthedicekambo/MainActivity2.kt
+++ app/src/main/java/com/example/rollthedicekambo/MainActivity2.kt
@@ -1,6 +1,11 @@
 package com.example.rollthedicekambo
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,5 +17,28 @@ class MainActivity2 : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main2)
 
+        val textViewIndovina = findViewById<TextView>(R.id.textViewIndovina)
+        val numeroUtente = findViewById<EditText>(R.id.numeroUtente)
+        val btnRoll = findViewById<Button>(R.id.btnRoll)
+        btnRoll.setOnClickListener{
+            val mioRandom = random() //genero un numero random
+            val nStringa = numeroUtente.text.toString()
+            val input = nStringa.toIntOrNull()
+            if(input != null){
+                if(input > 0 && input <=6)
+                    intent(mioRandom, input)
+            }
+        }
+    }
+
+    private fun random(): Int{
+        return (1..6).random()
+    }
+
+    private fun intent(mioRandom: Int, input: Int){
+        val mioIntent = Intent(this, MainActivity3::class.java)
+        mioIntent.putExtra("RANDOM", mioRandom)
+        mioIntent.putExtra("INPUT", input)
+        startActivity(mioIntent)
     }
 }
